aa4174e6d4caf9e94d501c0cf17ed27389712b73
bh1995 <55191733+bh1995@users.noreply.github.com>
2019-10-04 17:07:50 +0200

    "vefef"

--- webapi/R/lab5.R
+++ webapi/R/lab5.R
@@ -7,4 +7,10 @@ df_mun <- GET("http://api.kolada.se/v1/municipality")
 #make dataframe into list.
 ls_mun <- content(df_mun)
 
+df_kpi <- GET("http://api.kolada.se/v1/kpi")
 
+ls_kpi <- content(df_kpi)
+
+df_kost <- GET("http://api.kolada.se/v1/kpi/grundskola%20kostnad")
+
+ls_kost <- content(df_kost)
---
+++ webapi/R/shiny_stuff/ui.R
@@ -0,0 +1,12 @@
+library(shiny)
+
+# Define UI for the application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Showing kolada data / Lab 5"),
+  
+  sidebarPanel(),
+  
+  mainPanel()
+))
