3365b1393312188f30d8dd9beda037080adbc53f
bh1995 <55191733+bh1995@users.noreply.github.com>
2019-10-04 17:37:47 +0200

    "added example"

---
+++ webapi/R/get_rkaApp.R
@@ -0,0 +1,11 @@
+#' This is an example of the shiny app.
+#' 
+#' @importFrom  shiny runApp
+#' @importFrom utils installed.packages
+#' @export
+
+get_rka <- function(){
+ 
+  directions <- base::system.file("app", "app.R", package = "lab5") 
+  shiny::runapp(directions, display.mode = "normal")
+}
