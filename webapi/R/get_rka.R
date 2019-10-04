2012b174cd528fcb268908e1b9a4185622587d94
mahhaq <mahbuba.haque@ki.se>
2019-10-04 14:21:19 +0200

    "Plot"

--- webapi/R/get_rka.R
+++ webapi/R/get_rka.R
@@ -22,24 +22,27 @@ get_rka <- function(city){
   parsed <- jsonlite::fromJSON(content(rka_js,"text"), simplifyVector = FALSE)
   
   df <- data.frame(parsed$values[[1]])
-
-  return(df)
+  
+  rka2 <- GET(paste("http://api.kolada.se/v1/data/permunicipality/N60946/",df[,1], sep=""))
+  parsed2 <- jsonlite::fromJSON(content(rka2,"text"), simplifyVector = FALSE)
+  
+  newparsed <- parsed2[-2]
+  salaries <- matrix(unlist(newparsed), ncol = 6, byrow = TRUE)
+  Years <- salaries[,3]
+  b <- salaries[,5]
+  c <- salaries[,6]
+  
+  theplot <- function(){
+    plot(Years,b, ylab = "Income", ylim = c(0,400000), type="l", yaxt="n", col = 4)
+    lines(Years,c, col =2) 
+    axis(2,cex.axis=0.9) 
+    legend(2010,140000, legend=c("Men", "Women"), col=c("blue", "red"), lty=1:2, cex=0.8)
+  }
+  return(theplot())
 }
 
-#library("httr")
-#library("jsonlite")
 
-#gives a dataframe for the municipality data.
-#df_mun <- GET("http://api.kolada.se/v1/municipality")
-
-#make dataframe into list.
-#ls_mun <- content(df_mun)
-
-
-
-
-
-
+get_rka("Stockholm")
 
 
 
