f6a10cb6e2f46b694d199d44c14fd839edb9750b
bh1995 <55191733+bh1995@users.noreply.github.com>
2019-10-06 14:34:18 +0200

    "added some tests"

--- webapi/tests/testthat/test_rka.R
+++ webapi/tests/testthat/test_rka.R
@@ -10,5 +10,25 @@ context("test_rka.R")
     expect_is(test_rka, "list")
   })
   
-
+  test_that("The web API is not correct", {
+    expect_error(check_API("http://api.kolada.se/v1/data/permunicipality/"))
+  })
+  
+  test_that("get_rka gives correct output", {
+    test_rka <- unlist(get_rka(city = "Stockholm", "25-44"))
+    expect_equal(test_rka$rect$w, 8.493575)
+    expect_output(print(test_rka$rect$top),"140000" )
+  })
+  
+  test_that("get_rka gives correct output", {
+    test_rka <- get_rka("Uppsala", "25-44")
+    expect_true(all(test_rka$rect$h %in% 106817.8))
+    expect_true(all(test_rka$rect$left %in% 2010))
+  })
+  
+  test_that("get_rka gives worng output", {
+    test_rka <- get_rka("Uppsala", "25-44")
+    expect_false(all(test_rka$rect$h %in% 106818.8))
+    expect_false(all(test_rka$rect$left %in% 2011))
+  })
   
