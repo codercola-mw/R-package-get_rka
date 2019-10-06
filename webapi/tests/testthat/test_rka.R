1b9c12fe8b6351eb806c25b55c75ddd7efd2bfcd
Mahbuba Haque <mahbuba.haque@ki.se>
2019-10-06 16:18:37 +0200

    "tests"

--- webapi/tests/testthat/test_rka.R
+++ webapi/tests/testthat/test_rka.R
@@ -14,21 +14,23 @@ context("test_rka.R")
     expect_error(check_API("http://api.kolada.se/v1/data/permunicipality/"))
   })
   
-  test_that("get_rka gives correct output", {
+  test_that("get_rka gives correct plot attribute names", {
     test_rka <- unlist(get_rka(city = "Stockholm", "25-44"))
-    expect_equal(test_rka$rect$w, 8.493575)
-    expect_output(print(test_rka$rect$top),"140000" )
+    expect_true(names(test_rka)[1] == "rect.w")
+    expect_true(names(test_rka)[5] == "text.x1")
   })
   
   test_that("get_rka gives correct output", {
     test_rka <- get_rka("Uppsala", "25-44")
-    expect_true(all(test_rka$rect$h %in% 106817.8))
     expect_true(all(test_rka$rect$left %in% 2010))
+    expect_output(print(test_rka$rect$top),"140000")
   })
   
-  test_that("get_rka gives worng output", {
+  test_that("get_rka gives wrong output", {
     test_rka <- get_rka("Uppsala", "25-44")
     expect_false(all(test_rka$rect$h %in% 106818.8))
     expect_false(all(test_rka$rect$left %in% 2011))
   })
+  
+  
   
